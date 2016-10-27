0b704b1912d4701274d6941f6560063300ca1521
andersonpablo <mib_anderson@hotmail.com>
2016-10-27 11:02:23 -0300

    Update add.php

--- arduino_webclient/add.php
+++ arduino_webclient/add.php
@@ -3,7 +3,7 @@
 	include('conexao.php');
 
 	$stmt = $conn->prepare('INSERT INTO tempLog (quant_pessoas) VALUES ( :valor)');
-	$stmt->bindParam(':sensor1', $_GET['valor'], PDO::PARAM_INT);
+	$stmt->bindParam(':quant_pessoas', $_GET['valor'], PDO::PARAM_INT);
 	$stmt->execute();
 
 	echo "ok";
