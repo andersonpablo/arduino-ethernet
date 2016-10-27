4ac48a065d4f7b7e236362761245b7ada61aa16d
andersonpablo <mib_anderson@hotmail.com>
2016-10-27 10:45:31 -0300

    Atualização

--- arduino_webclient/add.php
+++ arduino_webclient/add.php
@@ -1,11 +1,9 @@
 <?php 
-	if(!isset($_GET['sensor1']) || !isset($_GET['sensor2']) || !isset($_GET['pessoas'])) die('fail');
+	if(!isset($_GET['valor'])) die('fail');
 	include('conexao.php');
 
-	$stmt = $conn->prepare('INSERT INTO logs (sensor1, sensor2, pessoas) VALUES ( :sensor1, :sensor2, :pessoas)');
-	$stmt->bindParam(':sensor1', $_GET['sensor1'], PDO::PARAM_INT);
-	$stmt->bindParam(':sensor2', $_GET['sensor2'], PDO::PARAM_INT);
-	$stmt->bindParam(':pessoas', $_GET['pessoas'], PDO::PARAM_INT);
+	$stmt = $conn->prepare('INSERT INTO tempLog (quant_pessoas) VALUES ( :valor)');
+	$stmt->bindParam(':sensor1', $_GET['valor'], PDO::PARAM_INT);
 	$stmt->execute();
 
 	echo "ok";
