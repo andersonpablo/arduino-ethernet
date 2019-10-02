2b5b8f9cd0b5d61cbd3f12525449ecf676c3a7b0
andersonpablo <mib_anderson@hotmail.com>
2019-10-02 02:46:55 -0300

    Update add.php

--- arduino_webclient/add.php
+++ arduino_webclient/add.php
@@ -1,7 +1,7 @@
 <?php include('conexao.php');
 
-error_reporting(0);
-ini_set(“display_errors”, 0);
+//error_reporting(0);
+//ini_set(“display_errors”, 0);
    
    $query = "INSERT INTO tempLog (quant_pessoas, id_arduino) VALUES ('".$_GET["valor"]."','".$_GET["id_arduino"]."')"; 
    $exec = $conn->exec($query);
